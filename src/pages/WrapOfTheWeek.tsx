52089cfbeb9779f3cb10ac81750da373cbb9f5f3
Wren <wren@wrapcommandai.com>
2026-02-08 18:09:26 +0000

    fix: WrapOfTheWeek SVG data URL causing build failure

--- src/pages/WrapOfTheWeek.tsx
+++ src/pages/WrapOfTheWeek.tsx
@@ -269,10 +269,8 @@ export default function WrapOfTheWeek() {
       <div className="w-full max-w-6xl mx-auto space-y-8">
         {/* Hero Header */}
         <div className="relative text-center py-8 px-4 rounded-2xl bg-gradient-to-br from-black via-fuchsia-950/30 to-black border border-fuchsia-500/30 overflow-hidden">
-          {/* Background Pattern */}
-          <div className="absolute inset-0 opacity-10">
-            <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width=\"60\" height=\"60\" viewBox=\"0 0 60 60\" xmlns=\"http://www.w3.org/2000/svg\"%3E%3Cg fill=\"none\" fill-rule=\"evenodd\"%3E%3Cg fill=\"%23ffffff\" fill-opacity=\"0.4\"%3E%3Cpath d=\"M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z\"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] bg-repeat" />
-          </div>
+          {/* Background Pattern - subtle grid */}
+          <div className="absolute inset-0 opacity-10 bg-[linear-gradient(rgba(255,255,255,0.1)_1px,transparent_1px),linear-gradient(90deg,rgba(255,255,255,0.1)_1px,transparent_1px)] bg-[size:20px_20px]" />
 
           {/* ClubWPW Logo */}
           <div className="relative">
